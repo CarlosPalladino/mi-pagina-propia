f335d261a99940375b899a4b7fc0b88e9557ae58
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-07-15 23:06:07 -0300

    primeras cosa

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 app.use(express.static('public'));
 app.listen(4000,()=>console.log('Abriendo página'));
-app.get('/',(req, res)=>res.send('hola mundo desde express'));
+app.get('/',(req, res)=>res.sendFile (path.resolve(__dirname, "./views/home.html")));
